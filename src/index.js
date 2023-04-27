aa862be7a09056ae0146e20ec7848f2047b36e9f
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-04-27 10:26:46 +0200

    update commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import axios from "axios";
+import { fetchImg } from "./fetchImg";
+import Notiflix from 'notiflix';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY_API = '35752200-55fbc3ad9b98a84c3d01ddaf0';
+
 const form = document.querySelector('.search-form')
 const queryInput = document.querySelector('input[name="searchQuery"]')
 
-const params = {
-  q: queryInput,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-};
-
-
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+   fetchImg(queryInput)
+   .then((result) => {
+    console.log(result);
+   })
+   .catch((err) => {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+   });
+})
