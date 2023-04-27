0576691c39b5bb924fac6b4f8e973eac04333e47
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-04-27 08:27:13 +0000

    Deploying to gh-pages from @ andrpavl/goit-js-hw-11@aa862be7a09056ae0146e20ec7848f2047b36e9f 🚀

--- index.82c507f0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form"),document.querySelector('input[name="searchQuery"]');
-//# sourceMappingURL=index.82c507f0.js.map
--- index.fe277b66.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form"),document.querySelector('input[name="searchQuery"]');
-//# sourceMappingURL=index.fe277b66.js.map
