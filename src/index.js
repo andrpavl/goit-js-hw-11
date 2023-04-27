0651cc17b0e161daec3909f1d3179999a21a4100
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-04-27 08:41:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
 
-const axios = require('axios');
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '35752200-55fbc3ad9b98a84c3d01ddaf0';
 const form = document.querySelector('.search-form')
@@ -14,11 +12,3 @@ const params = {
 };
 
 
-const resp = axios
-.get(`${BASE_URL}?${KEY_API}&${params}`)
-.then((response) => {
-  console.log(response)
-})
-.catch(
-  (err) => console.log(err)
-);
